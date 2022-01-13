d7db56514d9982ebad25d410b8d89461ee529909
Bayel <bayel.bakytovich@gmail.com>
2022-01-13 18:43:51 +0600

    removed IOExceprions in Main classes

--- week1/1047/src/Main.java
+++ week1/1047/src/Main.java
@@ -1,14 +1,8 @@
-import java.io.IOException;
 import java.util.Scanner;
-/**
- * IMPORTANT:
- *      O nome da classe deve ser "Main" para que a sua solução execute
- *      Class name must be "Main" for your solution to execute
- *      El nombre de la clase debe ser "Main" para que su solución ejecutar
- */
+
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         Scanner in = new Scanner(System.in);
         int beginHour = in.nextInt();
         int beginMinut = in.nextInt();
